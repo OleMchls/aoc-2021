2d721af494fa0285990e9f16078d3b6ee945ee09
Ole Michaelis <Ole.Michaelis@googlemail.com>
2021-12-07 15:04:06 +0100

    refactor use more fn composition

--- app/day7.hs
+++ app/day7.hs
@@ -10,8 +10,8 @@ prepare :: String -> Input
 prepare x = map read (words [if c == ',' then ' ' else c | c <- x])
 
 solve :: Input -> Int
-solve x = sum (map (\pos -> fuleSpend destination pos) x)
-    where destination = floor (average x)
+solve x = (sum . map (fuleSpend destination)) x
+    where destination = (floor . average) x
 
 fuleSpend :: Pos -> Pos -> Fuel
 fuleSpend a b = sum [1 .. abs (a - b)]
